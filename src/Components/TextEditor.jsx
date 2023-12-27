595004ef75641297969fc961876dc2768a8be98c
Fardeen Ahamed <1si20et403@sit.ac.in>
2023-12-27 20:21:08 +0530

    toolbar created

--- src/Components/TextEditor.jsx
+++ src/Components/TextEditor.jsx
@@ -1,14 +1,8 @@
 import React, { useEffect, useState } from "react";
-import {
-  Editor,
-  EditorState,
-  ContentState,
-  RichUtils,
-  convertToRaw,
-  convertFromRaw,
-} from "draft-js";
+import { Editor, EditorState, convertToRaw, convertFromRaw } from "draft-js";
 import "draft-js/dist/Draft.css";
 import Header from "./Header";
+import Toolbar from "./Toolbar";
 const TextEditor = () => {
   const [editorState, setEditorState] = useState(() => {
     const savedContent = localStorage.getItem("editorContent");
@@ -23,6 +17,9 @@ const TextEditor = () => {
   }, [editorState]);
   const submitHandler = (e) => {
     e.preventDefault();
+    const contentState = editorState.getCurrentContent();
+    const rawContent = convertToRaw(contentState);
+    localStorage.setItem("editorContent", JSON.stringify(rawContent));
   };
   const resetHandler = (e) => {
     e.preventDefault();
@@ -32,6 +29,9 @@ const TextEditor = () => {
   return (
     <>
       <Header submitHandler={submitHandler} resetHandler={resetHandler} />
+      <div className="bg-slate-500 p-2 w-[80vw] mx-auto my-2 rounded">
+        <Toolbar editorState={editorState} setEditorState={setEditorState} />
+      </div>
       <div className="editorContainer h-[60vh] w-[80vw] m-auto border-2 border-cyan-500  p-2">
         <Editor
           placeholder="type text here..."
@@ -42,5 +42,4 @@ const TextEditor = () => {
     </>
   );
 };
-
 export default TextEditor;
